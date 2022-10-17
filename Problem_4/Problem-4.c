712df918e09e91e99821835d5c6dce0fa79d3f49
shanto360 <shanto.360@yahoo.com>
2022-10-18 01:23:25 +0600

    Problem_4_subtraction_multiplication

--- Problem_4/Problem-4.c
+++ Problem_4/Problem-4.c
@@ -2,11 +2,17 @@
 #include<stdio.h>>
 int main()
 {
-    int a, b, summation;
+    int a, b, summation, subtraction, multiplication;
     printf("Enter number: ");
     scanf("%d%d", &a,&b);
     // calculating summation
     summation = a + b;
-    printf("The sum is: %d", summation);
+    // calculating subtraction
+    subtraction = a - b;
+    // calculating multiplication
+    multiplication = a * b;
+    printf("The summation is: %d \n", summation);
+    printf("The subtraction is: %d \n", subtraction);
+    printf("The subtraction is: %d", multiplication);
     return 0;
 }

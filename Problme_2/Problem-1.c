faefa76ea1bd1cbc36ffb5aa5fbf4e2d3a83a072
shanto360 <shanto.360@yahoo.com>
2022-10-18 00:05:25 +0600

    Problem_2

--- Problme_2/Problem-1.c
+++ Problme_2/Problem-1.c
@@ -1,1 +1,8 @@
 // Write a C program to declare two integer variables and set values 10 and 20 respectively. Now print the second
+#include<stdio.h>
+int main()
+{
+    int number_1 = 10, number_2 = 20;
+    printf("%d%d.",number_2,number_1);
+    return 0;
+}

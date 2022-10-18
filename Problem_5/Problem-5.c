ce2ca0da7a5ad6a43c03f41d29381f3e6a7638ce
shanto360 <shanto.360@yahoo.com>
2022-10-18 12:11:24 +0600

    Problem_5_Solved

--- Problem_5/Problem-5.c
+++ Problem_5/Problem-5.c
@@ -1,1 +1,10 @@
 // Write a C program to take a character variable and input and show the corresponding ASCII value of that character
+#include<stdio.h>
+int main()
+{
+    int a ;
+    printf("Enter a Decimal Value: ");
+    scanf("%d",&a);
+    printf("The ASCI value is: %c", a);
+    return 0;
+}
